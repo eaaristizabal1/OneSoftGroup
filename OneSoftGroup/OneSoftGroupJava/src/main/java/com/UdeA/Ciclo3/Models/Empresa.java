386ae7c2d66fc5641e0a4f847f58840e16e8af44
John Felipe Cardona López <johnfelipec@hotmail.com>
2022-09-28 20:33:29 -0500

    Actualizacion spring 4

--- OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/Models/Empresa.java
+++ OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/Models/Empresa.java
@@ -1,26 +1,17 @@
 package com.UdeA.Ciclo3.Models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name="Empresa")
 public class Empresa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnoreProperties({"empleado"})
-    private int emp_id;
+    private int id;
     private String nombre;
     private String direccion;
     private String telefono;
     private String NIT;
-
-    @OneToMany(targetEntity = Empleado.class ,fetch = FetchType.LAZY,cascade = {CascadeType.REMOVE},orphanRemoval = true)
-    @JoinColumn(name = "empresa_id", referencedColumnName = "emp_id" )
-    private List<Empleado> empleados = new ArrayList<>();
 
     public Empresa() {
     }
@@ -33,11 +24,11 @@ public class Empresa {
     }
 
     public int getId() {
-        return emp_id;
+        return id;
     }
 
     public void setId(int id) {
-        this.emp_id = id;
+        this.id = id;
     }
 
     public String getNombre() {
--- OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/OneSoftGroupApplication.java
+++ OneSoftGroup/OneSoftGroupJava/src/main/java/com/UdeA/Ciclo3/OneSoftGroupApplication.java
@@ -3,22 +3,21 @@ package com.UdeA.Ciclo3;
 import com.UdeA.Ciclo3.Models.Empresa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class OneSoftGroupApplication {
 
 	@GetMapping("/hello")
 	public String hello(){
-		return "Hola Ciclo 3!";
+		return "Hola Ciclo 3... Saldremos vivos de esto!";
 	}
 
 	@GetMapping("/test")
 	public String test(){
-		Empresa emp = new Empresa("OneSoftMarket LTDA", "Calle 44a Sur", "3044667968","800212002-3");
-		emp.setNombre("OneSoftMarket LTDA");
+		Empresa emp = new Empresa("SOLAR SAS", "Calle la geta", "3213213211","800212132-3");
+		emp.setNombre("SOLAR LTDA");
 		return emp.getNombre();
 	}
 
